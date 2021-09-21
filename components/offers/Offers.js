be960d24e4befd7676f1c8eac1f3c820c23165d2
Delside <ddelside@gmail.com>
2021-09-21 21:45:23 +0200

    created basic offers list view

---
+++ components/offers/Offers.js
@@ -0,0 +1,21 @@
+import Offer from "./Offer";
+import { Card } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import Link from 'next/link'
+
+function Offers(props) {
+    return (
+        <Container>
+        {props.offers.map((offer) => (
+            <Link href="[offerId].js">
+            <Card>
+                <p><strong>{offer.title}</strong>, {offer.salary_from} - {offer.salary_to} {offer.currency}</p>
+                <p>{offer.city}, {offer.street}</p>
+            </Card>
+            </Link>
+        ))}
+        </Container>
+    );
+}
+
+export default Offers
--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import Offer from '../components/offers/Offer';
 import NavigationBar from '../components/navbar/NavigationBar';
+import Offers from '../components/offers/Offers';
 
 export async function getStaticProps() {
   const res = await axios.get('https://test.justjoin.it/offers');
   const offers = res.data
   return {
     props: {
-      offers,
+      offers: offers.map(offer => ({
+        title: offer.title,
+        city: offer.city,
+        street: offer.street,
+        category: offer.marker_icon,
+        seniority: offer.experience_level,
+        salary_from: offer.salary_from,
+        salary_to: offer.salary_to,
+        company: offer.company_name,
+        id: offer.id,
+        currency: offer.salary_currency
+      })),
     },
     revalidate: 1
   }
 }
 
-function Home() {
+function Home(props) {
   return (
   <Container>
-    <NavigationBar />, 
-    <Offer />
+    <NavigationBar /> 
+    <Offers offers={props.offers}/>
   </Container>
   );
 }
